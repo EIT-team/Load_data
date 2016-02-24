21dd0e54b2cec31cd857c3b067bcb4ef369a1d64
Jimbles <j.avery.10@ucl.ac.uk>
2016-02-24 09:29:28 +0000

    started processbatch and perchannel demod

--- src/matlab/ScouseTom_ReadandDemodChn.m
+++ src/matlab/ScouseTom_ReadandDemodChn.m
@@ -2,36 +2,46 @@ function [ output_args ] = ScouseTom_ReadandDemodChn( HDRin,B,A,StartSec,StopSec
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 
-
+%% Fiddling with inputs
 HDR=HDRin;
 
 
 if strcmp(HDR.TYPE,'BDF');
     %biosemi has status channel as a separate chn in file
-Nchn=HDR.NS -1;
+    Nchn=HDR.NS -1;
 else
     Nchn=HDR.NS;
 end
 
-
+%% Check stuff
 Nsec=HDR.Nrec;
 
 if StopSec > NSec
     error('Stop sec is too long!');
 end
 
+
+%% Read and Demod each channel
 for iChn=1:Nchn
     
     HDR.NS=iChn;
     
     V=sread(HDR,StartSec,StopSec);
+    [ Vdata_demod,Pdata_demod ] = ScouseTom_data_DemodHilbert( V,B,A);
+    [Vmag,Phase]=ScouseTom_
+   
     
+   %triggers should be in from [start1, stop1; start2,stop2;...]
+   
+   %for loop through each trigger couple, and mean and trim each block
+    
+    
+end
 
 
+%% reshape into each one
 
-
-
-
+%
 
 end
 
