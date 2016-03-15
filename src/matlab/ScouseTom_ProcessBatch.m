ce7f790aa70201bcd19d8bfa58facaa2cad1592b
Jimbles <j.avery.10@ucl.ac.uk>
2016-03-15 16:32:50 +0000

    added .eeg files intro processbatch

--- src/matlab/ScouseTom_ProcessBatch.m
+++ src/matlab/ScouseTom_ProcessBatch.m
@@ -1,4 +1,4 @@
-function [ output_args ] = ScouseTom_ProcessBatch( dirname )
+function [ ] = ScouseTom_ProcessBatch( dirname )
 %SCOUSETOM_PROCESSBATCH Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -10,51 +10,70 @@ if exist('dirname','var') ==0
     if isempty(dirname)
         error('User Pressed Cancel');
     end
-    
 end
-%% find all the .bdfs in the directory
+%% find all the eeg files in the directory
 
-files=dir([dirname filesep '*.bdf']);
+bdffiles=dir([dirname filesep '*.bdf']);
+eegfiles=dir([dirname filesep '*.eeg']);
 
 % check if there are scans actually found
-if isempty(files)
-    error('No scan files found!');
+if isempty(bdffiles) && isempty(eegfiles)
+    error('No eeg or bdf files found!');
 end
 
-nfiles=length(files);
+nbdffiles=length(bdffiles);
+neegfiles=length(eegfiles);
 
-disp(['Found ' num2str(nfiles) ' .bdf files in directory']);
+disp(['Found ' num2str(nbdffiles) ' .bdf files in directory']);
+disp(['Found ' num2str(neegfiles) ' .eeg files in directory']);
 
 %ignore small files <1Mb as these are empty
 
-smallfile=cell2mat({files.bytes})/1e6;
-smallfile = smallfile < 1;
+smallbdffile=cell2mat({bdffiles.bytes})/1e6;
+smallbdffile = smallbdffile < 1;
 
-if any(smallfile)
-    disp(['WARNING! ' num2str(nnz(smallfile)) ' VERY SMALL (<1 Mb) file(s) were detected! These will be ignored']);
-    files(smallfile)=[];
-    
+if any(smallbdffile)
+    fprintf(2,'WARNING! %d VERY SMALL (<1 Mb) bdf file(s) were detected! These will be ignored\n',num2str(nnz(smallbdffile)));
+    bdffiles(smallbdffile)=[];
 end
 
-nfiles=length(files);
+smalleegfile=cell2mat({bdffiles.bytes})/1e6;
+smalleegfile = smalleegfile < 1;
+
+if any(smalleegfile)
+    fprintf(2,'WARNING! %d VERY SMALL (<1 Mb) eeg file(s) were detected! These will be ignored\n',num2str(nnz(smalleegfile)));
+    bdffiles(smalleegfile)=[];
+end
+
+nbdffiles=length(bdffiles);
+neegfiles=length(eegfiles);
 
 %% process each bdf!
-
-tic
-for iFile =1:nfiles
-    disp(['Processing file ' num2str(iFile) ' of ' num2str(nfiles) ': ' files(iFile).name]);
-    
-    ScouseTom_LoadBV(fullfile(dirname,files(iFile).name));
-    
-    disp('==================================');
-    
+if (nbdffiles > 0)
+    tic
+    for iFile =1:nbdffiles
+        disp(['Processing bdf file ' num2str(iFile) ' of ' num2str(nbdffiles) ': ' bdffiles(iFile).name]);
+        ScouseTom_LoadBV(fullfile(dirname,bdffiles(iFile).name));
+        disp('=========================================================');
+    end
+    el=toc;
+    fprintf('All .BDF Processing finished in : %.2f seconds\r\n',el);  
+end
+%% process each eeg
+if (neegfiles > 0)
+    tic
+    for iFile =1:neegfiles
+        disp(['Processing eeg file ' num2str(iFile) ' of ' num2str(neegfiles) ': ' eegfiles(iFile).name]);
+        ScouseTom_LoadBV(fullfile(dirname,eegfiles(iFile).name));
+        disp('=========================================================');
+    end
+    el=toc;
+    fprintf('All .EEG Processing finished in : %.2f seconds\n',el);  
 end
 
-el=toc;
+%% 
 
-fprintf('All Processing finished in : %.2f seconds\r\n',el);
-
-disp('AWW YISSSS');
+fprintf('ALL DONE! AWW YISSSS\n');
 
 
 
