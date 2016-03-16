18fa21088265a1806c9800740589d1460f291d7d
Jimbles <j.avery.10@ucl.ac.uk>
2016-03-16 09:26:29 +0000

    Updated LoadZ to look for AC files, and check for log file (when added to scousetom) 2

--- src/matlab/ScouseTom_LoadZ.m
+++ src/matlab/ScouseTom_LoadZ.m
@@ -13,7 +13,7 @@ function [ Zall ] = ScouseTom_LoadZ( varargin )
 
 if isempty(varargin) == 1
     
-    [filename, pathname] = uigetfile('*.bdf', 'Choose which Biosemi file with Zcheck to load');
+    [filename, pathname] = uigetfile({'*.bdf;*.eeg'}, 'Choose which EEG file with Zcheck to load- BioSemi or BrainVision');
     if isequal(filename,0) || isequal(pathname,0)
         error('User pressed cancel')
     else
@@ -43,40 +43,56 @@ if nargin == 3
     
 end
 
+%% Find the file type
+
+[pathstr,namestr,extstr] = fileparts(fname);
 
 %% Load HDR
 
-%if HDR not given then load it 
+%if HDR not given then load it
 if exist('HDR','var') == 0
-    HDR=ScouseTom_getbdfHDR(fname);
+    
+    %use function for correct file type
+    switch extstr
+        case '.bdf'
+            HDR=ScouseTom_getbdfHDR(fname);
+        case {'.eeg','.vhdr','.vmrk'}
+            HDR=ScouseTom_geteegHDR(fname);
+        otherwise
+            error('Unknown file type');
+    end
+    
 end
 
-if ~strcmp(HDR.TYPE,'BDF') 
-
+%Check HDR
+if ~any(strcmp(HDR.TYPE,{'BDF','BrainVision'}))
     error('BAD HDR FILE');
 end
-
 
 %% Get Triggers
 
 %if TT struct not given then obtain it from HDR
-
 if exist('TT','var') == 0
     Trigger= ScouseTom_TrigReadChn(HDR);
     TT= ScouseTom_TrigProcess(Trigger, HDR);
 end
 
-
 %% Get ExpSetup - or find from file
 
+%if ExpSetup not given then load the one. Or if cant be found then just
+%take defaults as it doesnt really matter that much!
+mfilename=fullfile(pathstr,[namestr '_log.mat']);
 
-%if ExpSetup not given then create one with the necessary info from HDR 
-
-if exist('ExpSetup','var') == 0
-    ExpSetup.Elec_num=HDR.NS-1; %number of electrodes
-    ExpSetup.Bad_Elec=[]; %bad electrodes assume none
-    ExpSetup.Desc='THIS IS A DUMMY EXPSETUP MADE DURING ZCHECK';
-
+if exist('ExpSetup','var') == 0 
+    %check if _log file is with eeg file
+    if exist(mfilename,'file')
+        load(mfilename);
+    else
+        fprintf(2,'Cannot find ExpSetup, so assuming defaults.this is probably fine\n');
+        ExpSetup.Elec_num=HDR.NS-1; %number of electrodes
+        ExpSetup.Bad_Elec=[]; %bad electrodes assume none
+        ExpSetup.Desc='THIS IS A DUMMY EXPSETUP MADE DURING ZCHECK';
+    end
 end
 
 
