99808feba191bb53d9a021e0a10edc176a9f9fa0
Jimbles <j.avery.10@ucl.ac.uk>
2017-05-09 18:50:42 +0100

    Added ScouseTom Examples

---
+++ example/ScouseTom_example.m
@@ -0,0 +1,19 @@
+% Example script to load Swisstom dataset
+% data is stored in the folder selected in a .mat
+% output is structure which contains 
+% all info about the file, as well as unscaled data etc.
+
+%% load Baseline
+
+%load boundary voltages of resistor phantom recording. Removing injected
+%channels, saturated channels, and plotting data
+[BVstruc]=ScouseTom_LoadBV('..\resources\data\ScouseTom\forreal_2_clean.bdf');
+
+%% load Baseline
+
+%load boundary voltages of resistor phantom recording. Removing injected
+%channels, saturated channels, and plotting data
+[BVstruc]=ScouseTom_LoadBV('..\resources\data\ScouseTom\MultiFreq.bdf');
+
+%%
+[Zout]=ScouseTom_LoadZ('..\resources\data\ScouseTom\S1a_Z4.bdf');
---
+++ resources/data/ScouseTom/readme.md
@@ -0,0 +1,3 @@
+# ScouseTom example data
+
+As the EEG systems used in the [ScouseTom](https://github.com/EIT-team/ScouseTom) are 24bit with a higher sampling rate, the datasets can get very large (> 100 Gb in some cases). So distribution through git lits the
