318fcb9d9d98f4ae284b815fa8204c63379ed9e1
Jimbles <j.avery.10@ucl.ac.uk>
2017-05-09 18:46:08 +0100

    Added KHU and SwissTom examples

---
+++ example/KHU_example.m
@@ -0,0 +1,17 @@
+% Example script to load KHU datasets
+% data is stored in the folder selected in a .mat
+% first output is corrected BV, second is the KHU structure which contains
+% all info about the file, aswell as unscaled data etc.
+
+%% load Baseline
+
+%load boundary voltages of baseline recording, cleaning the data and
+%plotting
+[BV_baseline, KHUs_baseline]=KHU_Load('..\resources\data\KHU\B1',1,1);
+
+%% load Perturbation
+
+%load boundary voltages of baseline recording, cleaning the data but not
+%plotting
+[BV_perturbation, KHUs_perturbation]=KHU_Load('..\resources\data\KHU\P1',1,0);
+
